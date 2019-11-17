50882e2e37df13a68e205d990bb043abb81a64b1
qchrisd <q.chris.d@gmail.com>
2019-11-17 15:27:12 -0500

    Adds functionality to email status reports

---
+++ emailStatus.py
@@ -0,0 +1,36 @@
+#
+# This program sends an email to the specified individual from the piabetes monitor email.
+# The email contains the status information of the current
+
+# Method to email the status of the monitor
+def sendEmail(message):
+	# Import packages
+	import smtplib, ssl
+	# Import config variables
+	import config
+
+	## Email server information
+	# Using Gmail with SSL encryption on port 465
+	port = 465
+	smtpServer = 'smtp.gmail.com'
+	sender = config.sender
+	pswd = config.pswd
+
+	# Receiver information
+	receiver = 'q.chris.d@gmail.com'
+
+	## Create connection
+	context = ssl.create_default_context()  # Creates context for ssl with default certs/authorization
+	with smtplib.SMTP_SSL(smtpServer, port, context=context) as server:
+		server.login(sender, pswd)
+		server.sendmail(sender, receiver, message)
+
+def formatEmail(subject, body):
+	message = "Subject: {}\n\n{}".format(subject, body)
+	return message
+
+
+
+if __name__ == '__main__':
+	sendEmail(formatEmail('test subject', 'test body'))
+#	sendEmail('test')
