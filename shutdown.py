b361163a8ac42631822521da01f8dc550b2bf94b
qchrisd <q.chris.d@gmail.com>
2019-11-26 20:31:46 -0500

    Adjusts the shutdown command to schedule shutdown for the next minute

--- shutdown.py
+++ shutdown.py
@@ -8,9 +8,9 @@ from subprocess import call
 
 # Shutdown method
 def shutdown():
-	call("sudo shutdown now --poweroff", shell = True)
+	call("sudo shutdown --poweroff", shell = True)
 
-
+# If this script is called directly then call the shutdown routine
 if __name__ == "__main__":
 	shutdown()
 
